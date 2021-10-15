8eacf2b8ed6b13efff755402683917e24fdebd5a
Ismael Avila <ismael.guada234015@gmail.com>
2021-10-14 22:45:31 -0500

    Definicion e implementacion de la clase Nodo

--- nodo.cpp
+++ nodo.cpp
@@ -1,6 +1,12 @@
 #include "nodo.h"
 
-Nodo::Nodo()
-{
+Nodo::Nodo() {}
 
+void Nodo::imprimir() {
+    std::cout << "\t" << nombre << std::endl;
+    std::cout << "\tEsta ubicado en el estado de " << estado << std::endl;
+    std::cout << "\tEs considerado un pueblo magico por la Secretaria de Turismo desde " << MagicoDesde << std::endl;
+    std::cout << "\tTiene un clima mayormente " << tipoClima << std::endl;
+    std::cout << "\tLa temperatura media es de " << tempMedia << "°C" << std::endl;
+    std::cout << "\tSu atraccion principal es " << atraccPrincipal << std::endl;
 }
--- nodo.h
+++ nodo.h
@@ -1,11 +1,23 @@
 #ifndef NODO_H
 #define NODO_H
 
+#include <string>
+#include <iostream>
 
-class Nodo
-{
-public:
+class Nodo {
+private:
+    std::string nombre;
+    std::string estado;
+    int MagicoDesde;
+    std::string tipoClima;
+    int tempMedia;
+    std::string atraccPrincipal;
+
     Nodo();
+
+    void imprimir();
+
+    friend class Grafo;
 };
 
 #endif // NODO_H
