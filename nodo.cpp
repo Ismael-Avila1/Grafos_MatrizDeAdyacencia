0d77db0ffc97c0b659b01ceaffd440b8456cf337
Ismael Avila <ismael.guada234015@gmail.com>
2021-10-16 14:02:09 -0500

    Cambio en el formato de impresion del nodo

--- nodo.cpp
+++ nodo.cpp
@@ -3,10 +3,11 @@
 Nodo::Nodo() {}
 
 void Nodo::imprimir() {
-    std::cout << "\t\t\t" << nombre << std::endl;
+    std::cout << "\t-----------------------------------------------------------------------------------";
+    std::cout << "\n\n\t\t\t" << nombre << std::endl;
     std::cout << "\tEsta ubicado en el estado de " << estado << std::endl;
     std::cout << "\tEs considerado un pueblo magico por la Secretaria de Turismo desde " << MagicoDesde << std::endl;
     std::cout << "\tTiene un clima mayormente " << tipoClima << std::endl;
-    std::cout << "\tLa temperatura media es de " << tempMedia << "°C" << std::endl;
+    std::cout << "\tLa temperatura media es de " << tempMedia << " grados Centigrados" << std::endl;
     std::cout << "\tSu atraccion principal es " << atraccPrincipal << std::endl;
 }
